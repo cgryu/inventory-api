eac7dede13cabccd56fd453332b077b00de54277
cgryu <christophergryu@gmail.com>
2026-07-19 18:41:39 -0400

    fixed function naming conventions and return status_codes for delete and post

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ items_db: dict[int, IteminDB] = {}
 itemID = 1
 taxRate = 0.05
 
-def calculate_tax(item: Item):
+def to_output(item: Item):
     return ItemOut(**item.model_dump(), price_with_tax=item.price*(1+taxRate))
 
 @app.get("/")
@@ -34,20 +34,20 @@ def read_root():
 def read_item(item_id: int):
     if item_id not in items_db:
         raise HTTPException(status_code=404, detail="Item not found")
-    return calculate_tax(items_db[item_id])
+    return to_output(items_db[item_id])
 
 @app.get("/items", response_model=list[ItemOut])
 def list_items(limit: int = 10, skip: int = 0):
-    return [calculate_tax(item) for item in list(items_db.values())[skip:skip+limit]]
+    return [to_output(item) for item in list(items_db.values())[skip:skip+limit]]
         
 
-@app.post("/items", response_model=ItemOut)
+@app.post("/items", response_model=ItemOut, status_code=201)
 def create_item(item: Item):
     global itemID
     items_db[itemID] = IteminDB(**item.model_dump())
     oldID = itemID
     itemID += 1
-    return calculate_tax(items_db[oldID])
+    return to_output(items_db[oldID])
 
 @app.put("/items/{item_id}", response_model=ItemOut)
 def update_item(item_id: int, item: Item):
@@ -55,15 +55,15 @@ def update_item(item_id: int, item: Item):
         raise HTTPException(status_code=404, detail="Item not found")
         
     items_db[item_id] = IteminDB(**item.model_dump())
-    return calculate_tax(items_db[item_id])
+    return to_output(items_db[item_id])
 
-@app.delete("/items/{item_id}")
+@app.delete("/items/{item_id}", status_code=204)
 def delete_item(item_id: int):
     if item_id not in items_db:
         raise HTTPException(status_code=404, detail="Item not found")
     
     items_db.pop(item_id)
-    return {"status": "ok"}
+    return None
 
 @app.patch("/items/{item_id}", response_model=ItemOut)
 def patch_item(item_id: int, update: ItemUpdate):
@@ -76,4 +76,4 @@ def patch_item(item_id: int, update: ItemUpdate):
     except ValidationError as e:
         raise HTTPException(status_code=422, detail=e.errors())
 
-    return calculate_tax(items_db[item_id])
+    return to_output(items_db[item_id])
