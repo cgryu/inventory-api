3ccbb95311ce925f59d58976e4ae67e73149bae5
cgryu <christophergryu@gmail.com>
2026-08-03 00:57:51 -0400

    added full CRUD functionality with a local postgresql database

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel
+import psycopg
+import os
 
 app = FastAPI()
+conn = psycopg.connect(
+    dbname="playground",
+    user="cgryu",
+    password=os.environ["DB_PASSWORD"],
+    host="localhost"
+)
 
 class Item(BaseModel):
     name: str
@@ -9,22 +17,18 @@ class Item(BaseModel):
     in_stock: bool = True
 
 class ItemOut(Item):
+    item_id: int
     price_with_tax: float
-
-class IteminDB(Item):
-    secret: str = "internal"
 
 class ItemUpdate(BaseModel):
     name: str | None = None
     price: float | None = None
     in_stock: bool | None = None
 
-items_db: dict[int, IteminDB] = {}
-itemID = 1
 taxRate = 0.05
 
-def to_output(item: Item):
-    return ItemOut(**item.model_dump(), price_with_tax=item.price*(1+taxRate))
+def to_output(item_id: int, item: Item):
+    return ItemOut(item_id=item_id, **item.model_dump(), price_with_tax=item.price*(1+taxRate))
 
 @app.get("/")
 def read_root():
@@ -32,48 +36,112 @@ def read_root():
 
 @app.get("/items/{item_id}", response_model=ItemOut)
 def read_item(item_id: int):
-    if item_id not in items_db:
+    with conn.cursor() as cur:
+        cur.execute(
+            "SELECT item_id, name, price, in_stock FROM items WHERE item_id = %s", 
+            (item_id,)
+        )
+        row = cur.fetchone()
+    if row is None:
         raise HTTPException(status_code=404, detail="Item not found")
-    return to_output(items_db[item_id])
+    saved = Item(name=row[1], price=row[2], in_stock=row[3])
+    return to_output(row[0], saved)    
 
 @app.get("/items", response_model=list[ItemOut])
 def list_items(limit: int = 10, skip: int = 0):
-    return [to_output(item) for item in list(items_db.values())[skip:skip+limit]]
-        
+    with conn.cursor() as cur:
+        cur.execute(
+            "SELECT item_id, name, price, in_stock FROM items ORDER BY item_id LIMIT %s OFFSET %s", (limit, skip)
+        )
+        rows = cur.fetchall()
+    return [to_output(item_id, Item(name=name, price=price, in_stock=in_stock)) for (item_id, name, price, in_stock) in rows]
 
 @app.post("/items", response_model=ItemOut, status_code=201)
 def create_item(item: Item):
-    global itemID
-    items_db[itemID] = IteminDB(**item.model_dump())
-    oldID = itemID
-    itemID += 1
-    return to_output(items_db[oldID])
+    try:
+        with conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO items (name, price, in_stock) VALUES (%s, %s, %s) RETURNING item_id, name, price, in_stock",
+                (item.name, item.price, item.in_stock)
+            )
+            row = cur.fetchone()
+            conn.commit()
+
+    except psycopg.errors.UniqueViolation:
+        conn.rollback()
+        raise HTTPException(status_code=409, detail="item already exists")
+    
+    saved = Item(name=row[1], price=row[2], in_stock=row[3])
+    return to_output(row[0], saved)
 
 @app.put("/items/{item_id}", response_model=ItemOut)
 def update_item(item_id: int, item: Item):
-    if item_id not in items_db:
-        raise HTTPException(status_code=404, detail="Item not found")
-        
-    items_db[item_id] = IteminDB(**item.model_dump())
-    return to_output(items_db[item_id])
+    try:
+        with conn.cursor() as cur:
+            cur.execute(
+                "UPDATE items SET name = %s, price = %s, in_stock =%s "
+                "WHERE item_id = %s "
+                "RETURNING item_id, name, price, in_stock",
+                (item.name, item.price, item.in_stock, item_id)
+            )
+            row = cur.fetchone()
+        if row is None:
+            conn.rollback()
+            raise HTTPException(status_code=404, detail="Item not found")
+        conn.commit()
+    except psycopg.errors.UniqueViolation:
+        conn.rollback()
+        raise HTTPException(status_code=409, detail="Name already exists")
+    item_id, name, price, in_stock = row
+    return to_output(item_id, Item(name=name, price=price, in_stock=in_stock))
 
 @app.delete("/items/{item_id}", status_code=204)
 def delete_item(item_id: int):
-    if item_id not in items_db:
+    with conn.cursor() as cur:
+        cur.execute(
+            "DELETE FROM items WHERE item_id = %s RETURNING item_id", 
+            (item_id,)
+        )
+        row = cur.fetchone()
+
+    if row is not None:
+        conn.commit()
+    else:
+        conn.rollback()
         raise HTTPException(status_code=404, detail="Item not found")
     
-    items_db.pop(item_id)
-    return None
 
 @app.patch("/items/{item_id}", response_model=ItemOut)
 def patch_item(item_id: int, update: ItemUpdate):
-    if item_id not in items_db:
-        raise HTTPException(status_code=404, detail="Item not found")
-    patch_data = update.model_dump(exclude_unset=True)
-    merged = items_db[item_id].model_dump() | patch_data
-    try:
-        items_db[item_id] = IteminDB(**merged)
-    except ValidationError as e:
-        raise HTTPException(status_code=422, detail=e.errors())
+    with conn.cursor() as cur:
+        cur.execute(
+            "SELECT item_id, name, price, in_stock FROM items WHERE item_id=%s",
+            (item_id,)
+        )
+        row = cur.fetchone()
 
-    return to_output(items_db[item_id])
+    if row is None:
+        conn.rollback()
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    old_data = Item(name=row[1], price=row[2], in_stock=row[3]).model_dump()
+    patch_data = update.model_dump(exclude_unset=True)
+    update_data = old_data | patch_data
+
+    try:
+        with conn.cursor() as cur:
+            cur.execute(
+                "UPDATE items SET name=%s, price=%s, in_stock=%s "
+                "WHERE item_id = %s "
+                "RETURNING item_id, name, price, in_stock",
+                (update_data["name"], update_data["price"], update_data["in_stock"], item_id)
+            )
+            row = cur.fetchone()
+        conn.commit()
+
+    except psycopg.errors.UniqueViolation:
+        conn.rollback()
+        raise HTTPException(status_code=409, detail="Name already exists")
+
+    item_id, name, price, in_stock = row
+    return to_output(item_id, Item(name=name, price=price, in_stock=in_stock))
